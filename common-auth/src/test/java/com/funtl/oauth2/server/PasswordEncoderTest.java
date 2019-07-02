8553ba2420ee879dbc54ed1759e9de1aad1611d9
向绍谷 <1069342827@qq.com>
2019-07-02 18:00:21 +0800

    初始化jdbc环境

---
+++ common-auth/src/test/java/com/funtl/oauth2/server/PasswordEncoderTest.java
@@ -0,0 +1,10 @@
+package com.funtl.oauth2.server;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoderTest {
+
+    public static void main(String[] args) {
+        System.out.println(new BCryptPasswordEncoder().encode("secret"));
+    }
+}
